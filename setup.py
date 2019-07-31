24e05454fc3a4d8f4269ea4db5fb1c61ccf4658e
Tom Holding <t.m.holding@exeter.ac.uk>
2019-07-31 07:46:03 +0100

    new subdirectory for package

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 30 15:15:53 2019
+
+@author: Tom Holding
+"""
+
+from distutils.core import setup;
+
+setup(
+    name='FluxEngine',
+    version='4.0dev',
+    packages=['fluxengine',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)

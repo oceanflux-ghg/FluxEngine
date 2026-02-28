dcf43b91606727f3112e4c74f40cf154fe283e91
Tom <tmholding42@gmail.com>
2026-02-28 17:32:13 +0100

    Fix typo in extras_require for dev dependencies

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setuptools.setup(
     package_data={"fluxengine": allPackageDataPaths},
 
     install_requires=dependencies,
-    extra_require={
+    extras_require={
         "dev": ["pytest>=9.0"]
         }
     );

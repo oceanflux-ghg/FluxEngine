420b7cac51e82874b6cd455823ec59cfd72bdbbb
THolding <tmholding42@gmail.com>
2026-03-20 18:03:52 +0100

    installing dev-dependencies, installing fluxengine in editable mode, example of custom GTV

--- fluxengine/test_data/example_custom_gtv.py
+++ fluxengine/test_data/example_custom_gtv.py
@@ -14,13 +14,14 @@ from fluxengine.core.datalayer import DataLayer
 #Used to test third-party execution of custom GTVs
 class k_CustomGtvTest(KCalculationBase):
     # Optional initialiser arguments can be used, but their names must correspond to names in the config file.
-    # For example some_parameter is used here.
+    # For example k_CustomGtvTest_some_parameter is used here.
     #Note: In the input arguments, it's best to append the parameterisation's
     # name or some other unique namespace to the arg names. This avoids polluting
     # the config/runParameter's namespace (minimises risk of name clashes)
     def __init__(self, k_CustomGtvTest_some_parameter):
         self.name = self.__class__.__name__
-        self.someParameter = k_CustomGtvTest_some_parameter  # 'example_init_parameter' would need to be defined in the configuration file
+        self.someParameter = k_CustomGtvTest_some_parameter  # 'k_CustomGtvTest_some_parameter' needs to be defined in the configuration file
+        #Note: the internal name (self.someParameter) can have any name it like and does not need to correspond to a configuration variable
 
     # Must return a list of strings corresponding to the input data layers required by the k calculations. These must already exist.
     def input_names(self):
